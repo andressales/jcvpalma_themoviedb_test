8c2b2a9d212f18f525f5164bead69c358733bfed
Julio C V Palma <jcvpalma@gmail.com>
2019-04-02 22:00:47 -0300

    inicializacao do projeto de js ts

---
+++ angular/jtmdtest/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { v_HttpInterceptorProviders } from './interceptors/inde';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    v_HttpInterceptorProviders
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
---
+++ angular/jtmdtest/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API_TMDB_KEY : "8129859277bb7335d5a2680aecda0a19",
+  API_AUTH_BEARER_TMDB : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTI5ODU5Mjc3YmI3MzM1ZDVhMjY4MGFlY2RhMGExOSIsInN1YiI6IjVjYTEyOGIwOTI1MTQxNTU3ODA1OTQwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.imAXrl8uqDjDwcrqozy6NwFkvz3P14VjEIZJVcpCFik"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
