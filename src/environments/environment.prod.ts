aee0ea7fba902ac16be384c1c924b7cd99f6492e
Julio C V Palma <jcvpalma@gmail.com>
2019-04-04 02:45:26 -0300

    atualizando exemplo teste incluindo minification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_TMDB_KEY : "8129859277bb7335d5a2680aecda0a19",
+  API_AUTH_BEARER_TMDB : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTI5ODU5Mjc3YmI3MzM1ZDVhMjY4MGFlY2RhMGExOSIsInN1YiI6IjVjYTEyOGIwOTI1MTQxNTU3ODA1OTQwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.imAXrl8uqDjDwcrqozy6NwFkvz3P14VjEIZJVcpCFik",
+  URL_TMDB_API : "https://api.themoviedb.org/3/"
 };
