67d6ab823185a5c785277ab286d9e2f8f9eed458
Julio C V Palma <jcvpalma@gmail.com>
2019-04-08 12:22:00 -0300

    update app.js to deploy in heroku

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(epress.static(__dirname+ '/dist/jtmdtest'));
+
+
+app.get('/*', function(req,res){
+    console.log(req);
+    res.sendFile(path.join(__dirname+'/dist/jtmdtest/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
